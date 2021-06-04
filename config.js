4ac858b36f5fcd76c68bfa3e5a2cd7c08ffa7376
mrbaondh <85336958+mrbaondh@users.noreply.github.com>
2021-06-04 14:46:19 +0700

    Update config.js

--- config.js
+++ config.js
@@ -9,10 +9,10 @@ const CONFIG = {
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Vẫn cứ là thích anh <33',
     btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại kết anh <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    reply: 'thích thì nhích mà không yêu thì cũng nhích <33333333',
+    mess: 'Anh biết mà 🥰. <3 em nhiều nhiều 😘😘',
     messDesc: 'Khi nào hết dịch anh mời cà phê nhé.',
     btnAccept: 'Okiiiii lun <3',
 }
